eb3f67b0c4e0d246102132ead9b3735b3f90f938
Durmuş Demirtaş <demirtasdurmus@gmail.com>
2024-08-24 12:13:02 +0300

    feat: add express middleware adaptor to be able to use middleware in Express.js ecosystem (#22)
    * feat: add express adaptor to be able to use express midlewares
    
    * test: add tests for express adaptor and new response methods
    
    * docs: update docs to include express middleware adaptor

---
+++ src/lib/express-middleware-adaptor.ts
@@ -0,0 +1,25 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { IMiddlewareHandler, NextHandler } from '../interfaces';
+import { CustomRequest } from './custom-request';
+import { CustomResponse } from './custom-response';
+
+/**
+ * This function is used to wrap an Express middleware function so that it can be used as a middleware handler in the API router.
+ * @param expressMiddleware The Express middleware function to be wrapped.
+ * @returns A middleware handler that can be used in the API router.
+ */
+export function expressMiddlewareAdaptor(
+  expressMiddleware: (req: any, res: any, next: (err?: any) => void) => void,
+): IMiddlewareHandler {
+  return async (req: CustomRequest, res: CustomResponse, next: NextHandler): Promise<void> => {
+    return new Promise<void>((resolve, reject) => {
+      expressMiddleware(req, res, (err) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(next());
+        }
+      });
+    });
+  };
+}
